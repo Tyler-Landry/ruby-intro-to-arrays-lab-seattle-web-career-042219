47849cd5259b60e2d711d29765c9719d4d4d3d32
Tyler Landry <tyler.landry1996@outlook.com>
2019-04-18 01:55:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,5 +1,5 @@
 def instantiate_new_array
-  x = Array.new
+  Array.new
 end
 
 def array_with_two_elements
@@ -7,3 +7,16 @@ def array_with_two_elements
 end
 
 def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  
